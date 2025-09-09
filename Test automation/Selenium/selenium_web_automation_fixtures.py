1a71ae9183ab929f5d7b21040e022dabc972e0b5
Anna <anna@MacBook-Air-von-Nutzerin1.local>
2025-09-09 21:44:44 +0200

    added fixtures and parametrization to Login with Selenium

--- Test automation/Selenium/selenium_web_automation_fixtures.py
+++ Test automation/Selenium/selenium_web_automation_fixtures.py
@@ -11,67 +11,54 @@ Part 2: Parametrisierung und Fixtures
 from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
-import time
+import pytest
 
-
+# -----------------------------
+# webdriver fixture
+# -----------------------------
 #setup the webdriver, open Chrome and the webpage
-driver = webdriver.Chrome()
-driver.get("https://www.saucedemo.com/")
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+    yield driver
+    driver.quit()
+
+
+# -----------------------------
+# password fixture
+# -----------------------------
+@pytest.fixture
+def password(driver):
+    # select container via XPath
+    password_node = driver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div[2]/div/div[2]")
+    password_text = password_node.text.splitlines()[-1]
+    return password_text
+
 
 # -----------------------------
 # get usernames
 # -----------------------------
-# get container by ID
-credentials_box = driver.find_element(By.ID, "login_credentials")
+def get_users():
+    driver = webdriver.Chrome()
+    driver.get("https://www.saucedemo.com/")
+    credentials_box = driver.find_element(By.ID, "login_credentials")
+    lines = credentials_box.text.splitlines()
+    users = [line.strip() for line in lines if line.strip().endswith("user")]
+    driver.quit()
+    return users
 
-# extract & trim the usernames supplied in the container
-lines = credentials_box.text.splitlines()
-users = [line.strip() for line in lines if line.strip().endswith("user")]
 
 # -----------------------------
-# get password
+# parametrized login function
 # -----------------------------
-# select container via CSS-Selector
-password_node = driver.find_element(By.CSS_SELECTOR, "#root > div > div:nth-child(2) > div:nth-child(2) > div > div:nth-child(2)")
-password = password_node.text
+@pytest.mark.parametrize("user", get_users())
+def test_login(driver, user, password):
+    login_box_username = driver.find_element(By.ID, "user-name")
+    login_box_password = driver.find_element(By.ID, "password")
 
+    login_box_username.send_keys(user)
+    login_box_password.send_keys(password)
 
-"""
-#find login fields
-login_box_username = driver.find_element(By.ID, "user-name")
-login_box_password = driver.find_element(By.ID, "password")
-
-#login with the provided credentials
-login_box_username.send_keys(USERNAME)
-login_box_password.send_keys(PASSWORD)
-
-#click Login button
-login_button = driver.find_element(By.ID, "login-button")
-login_button.click()
-
-time.sleep(2)
-
-#assert successful login
-webpage = driver.current_url
-assert webpage == "https://www.saucedemo.com/inventory.html", "Login not successful!"
-
-
-"""
-"""
-2. Produktprüfung:
-   - Nach Login das Produkt "Sauce Labs Backpack" auf der Seite suchen
-   - Assert, dass der Produktname angezeigt wird
-"""
-"""
-#find the desired article
-sauce_lab_backpack = driver.find_element(By.LINK_TEXT, "Sauce Labs Backpack")
-
-#assert the desired article is displayed
-assert sauce_lab_backpack != NoSuchElementException, "Article not found!"
-
-#close the browser
-driver.quit()
-
-
-
-"""
+    driver.find_element(By.ID, "login-button").click()
+    assert driver.current_url == "https://www.saucedemo.com/inventory.html", f"Login für {user} nicht erfolgreich!"
