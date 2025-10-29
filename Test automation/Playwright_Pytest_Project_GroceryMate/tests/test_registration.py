e6ea7a3fb4d953bfe46b95a9f9745b00d92e7893
anna_pohle <anna.pohle@posteo.de>
2025-10-29 15:44:23 +0100

    added test_registration.py

--- Test automation/Playwright_Pytest_Project_GroceryMate/tests/test_registration.py
+++ Test automation/Playwright_Pytest_Project_GroceryMate/tests/test_registration.py
@@ -3,6 +3,7 @@ from pages.authentification_page import AuthPage
 
 
 def test_create_account():
+    test_data = {"email":"", "password":"", "name":""}
     with sync_playwright() as playwright:
         # browser starten
         browser = playwright.chromium.launch(headless=False, slow_mo=50)
@@ -14,7 +15,10 @@ def test_create_account():
         auth_page = AuthPage(page)
 
         #methode create account nutzen
+        auth_page.create_account(test_data["email"], test_data["password"], test_data["name"])
 
         #methode login nutzen
+        auth_page.login()
 
         # prüfen ob sich die URL zur startseite ändert
+        assert page.url == "https://grocerymate.masterschool.com/
