71b2912f6748abe35779ccd596128b64e105ef8a
anna_pohle <anna.pohle@posteo.de>
2025-11-04 00:46:42 +0100

    completed checkout_page

--- Test automation/Playwright_Pytest_Project_GroceryMate/pages/checkout_page.py
+++ Test automation/Playwright_Pytest_Project_GroceryMate/pages/checkout_page.py
@@ -8,3 +8,30 @@ class CheckoutPage(BasePage):
 
     def __init__(self, page):
         super().__init__(page)
+        self.street_input = page.get_by_placeholder("Street Address")
+        self.city_input = page.get_by_placeholder("City")
+        self.postal_code_input = page.get_by_placeholder("Postal Code")
+        self.card_number_input = page.get_by_placeholder("Card number")
+        self.name_on_card_input = page.get_by_placeholder("Name on card")
+        self.expiration_input = page.get_by_placeholder("Expiration")
+        self.cvv_input = page.get_by_placeholder("Cvv")
+        self.buy_now_button = page.get_by_role("button", name="Buy now")
+
+    def fill_address(self, street: str, city: str, postal_code: str):
+        # Füllt die Adressfelder aus"
+        self.street_input.fill(street)
+        self.city_input.fill(city)
+        self.postal_code_input.fill(postal_code)
+
+    def fill_payment(self, card_number: str, name_on_card: str, expiration: str, cvv: str):
+        # Füllt die Zahlungsfelder aus
+        self.card_number_input.fill(card_number)
+        self.name_on_card_input.fill(name_on_card)
+        self.expiration_input.fill(expiration)
+        self.cvv_input.fill(cvv)
+
+    def complete_checkout(self, address_data: dict, payment_data: dict):
+        # Füllt alle Felder mit den Testdaten aus und schließt Checkout ab
+        self.fill_address(**address_data)
+        self.fill_payment(**payment_data)
+        self.buy_now_button.click()
