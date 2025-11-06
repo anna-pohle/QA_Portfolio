e9fe6ce7c03f42dffc294cecb0b12431c9597869
anna_pohle <anna.pohle@posteo.de>
2025-11-06 19:50:45 +0100

    refactored data handling in test_registration

--- Test automation/Playwright_Pytest_Project_GroceryMate/tests/test_registration.py
+++ Test automation/Playwright_Pytest_Project_GroceryMate/tests/test_registration.py
@@ -1,24 +1,38 @@
+import pytest
+
 from pages.auth_page import AuthPage
-from testdata_config import AuthData
-from system_config import EXTENDED_TIMEOUT
+from tests.testdata_config import AuthData
+from system_config import BASE_URL
 
 
 def test_create_account_and_login(browser_page):
-    #erfolgreiche Registrierung eines neuen Users
+    """
+    erfolgreiche Registrierung eines neuen Users + anschließender Login
+    1. Neuen User anlegen
+    2. Account registrieren
+    3. mit diesem Account anmelden
+    4. Prüfen ob Homepage geladen wird
+    """
 
     #Arrange
     auth_page = AuthPage(browser_page)
-    new_user = AuthData.NEW_USER
+    new_user = AuthData.new_user()
 
-    #Act & Assert 1
-    auth_page.create_account(**new_user)
-    assert "auth" in browser_page.url, "Nicht auf der Auth-Seite!"
+    #Act: Account erstellen (navigiert intern zur auth-page)
+    auth_page.create_account(
+        email=new_user.email,
+        password=new_user.password,
+        name= new_user.name
+    )
 
-    #Act & Assert 2
-    auth_page.login(
-            email=new_user["email"],
-            password=new_user["password"])
-    # prüfen ob sich die URL zur startseite ändert:
-    auth_page.page.wait_for_url("https://grocerymate.masterschool.com/", timeout=EXTENDED_TIMEOUT)
+    #Act: Login (ist nach create_account weiterhin auf Auth-Seite)
+    homepage = auth_page.login(
+            email=new_user.email,
+            password=new_user.password
+    )
 
-    assert "auth" not in browser_page.url, "Sollte nicht mehr auf Auth-Seite sein"
+    # Assert: prüfen ob die Homepage geladen wird
+    homepage.page.wait_for_url(BASE_URL)
+    assert homepage.page.url == BASE_URL, f"Erwartete URL: {BASE_URL}, aktuelle URL: {homepage.page.url}"
+
+
