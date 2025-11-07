3cfebb7bc43bb8d6267ade83d060f7bb2d4193a6
anna_pohle <anna.pohle@posteo.de>
2025-11-07 15:36:00 +0100

    refactored asserts and function-split in  test_registration

--- Test automation/Playwright_Pytest_Project_GroceryMate/tests/test_registration.py
+++ Test automation/Playwright_Pytest_Project_GroceryMate/tests/test_registration.py
@@ -1,38 +1,46 @@
-import pytest
-
 from pages.auth_page import AuthPage
 from tests.testdata_config import AuthData
 from system_config import BASE_URL
 
-
-def test_create_account_and_login(browser_page):
+def test_create_account(browser_page):
     """
-    erfolgreiche Registrierung eines neuen Users + anschließender Login
+    Testet erfolgreiche Registrierung eines neuen Users
     1. Neuen User anlegen
     2. Account registrieren
     3. mit diesem Account anmelden
-    4. Prüfen ob Homepage geladen wird
+    4. Prüfen, ob Toast angezeigt wird, dass die Registrierung erfolgreich war
+    """
+    # Arrange
+    auth_page = AuthPage(browser_page)
+    new_user = AuthData.new_user()
+
+    # Act: Account erstellen (navigiert intern zur auth-page)
+    auth_page.create_account(
+        email=new_user.email,
+        password=new_user.password,
+        name=new_user.name
+    )
+
+    # Assert
+    success_toast = browser_page.locator("text=Registration successful")
+    success_toast.wait_for(state="visible")
+    assert success_toast.is_visible(), "Success-Toast nicht angezeigt!"
+
+def test_login(browser_page):
+    """
+    Testet erfolgreichen Login mit existierendem User
+    1. navigiert zur Login-Seite
+    2. loggt sich mit einem existierenden User an
     """
 
     #Arrange
     auth_page = AuthPage(browser_page)
-    new_user = AuthData.new_user()
+    existing_user = AuthData.EXISTING_USER
 
-    #Act: Account erstellen (navigiert intern zur auth-page)
-    auth_page.create_account(
-        email=new_user.email,
-        password=new_user.password,
-        name= new_user.name
-    )
-
-    #Act: Login (ist nach create_account weiterhin auf Auth-Seite)
-    homepage = auth_page.login(
-            email=new_user.email,
-            password=new_user.password
-    )
+    #Act:
+    auth_page.go_to_login()
+    homepage = auth_page.login(existing_user.email, existing_user.password)
 
     # Assert: prüfen ob die Homepage geladen wird
     homepage.page.wait_for_url(BASE_URL)
     assert homepage.page.url == BASE_URL, f"Erwartete URL: {BASE_URL}, aktuelle URL: {homepage.page.url}"
-
-
